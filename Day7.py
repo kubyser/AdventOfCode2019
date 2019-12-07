7b4a498e4d74c5d1e7298e59bf13c5f23b459574
Sergey <kubiks@gmail.com>
2019-12-07 14:19:45 +0000

    Day7 pt1 done

--- Day7.py
+++ Day7.py
@@ -139,15 +139,15 @@ class Chain:
     def compute(self):
         for i in range(len(self.computers)):
             c = self.computers[i]
-            print("Computer ",i, " running. Input before preInput: ", c.inStream)
+#            print("Computer ",i, " running. Input before preInput: ", c.inStream)
             c.inStream = self.buildInput(c, i)
-            print("Computer ",i, " running. Input after preInput: ", c.inStream)
+#            print("Computer ",i, " running. Input after preInput: ", c.inStream)
             if i<len(self.computers)-1:
                 self.buffer = []
                 c.outStream = self.buffer
                 self.computers[i+1].inStream = self.buffer
             c.compute()
-            print("Buffer after run of computer ", i, ": ", self.buffer)
+#            print("Buffer after run of computer ", i, ": ", self.buffer)
             i = i+1
 
 def makeListOfLists(s):
@@ -158,24 +158,54 @@ def makeListOfLists(s):
         r.append(n)
     return r
 
+def readProgram(fileName):
+    f = open(fileName, "r")
+    p = [int(x) for x in f.read().split(",")]
+    f.close()
+    return p
+
 #p=[3,0,4,0,99]
 #p=[1105,1,10,0,0,0,0,0,0,0,3,0,4,0,1005,0,10,99]
 #p=[3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0] #43210
 #p=[3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0] #01234
-p=[3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0] #10432
+#p=[3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0] #10432
 #p=[1101,1,5,0,4,0,99]
+
+p=readProgram("day7_input.txt")
 inStream = [0]
 
+maxThrust = 0
+bestPhases = []
 for p1 in range(5):
+    used = {p1}
     for p2 in range(5):
+        if p2 in used:
+            continue
+        used.add(p2)
         for p3 in range(5):
+            if p3 in used:
+                continue
+            used.add(p3)
             for p4 in range(5):
+                if p4 in used:
+                    continue
+                used.add(p4)
                 for p5 in range(5):
-                    break
+                    if p5 in used:
+                        continue
+                    phases = makeListOfLists([p1, p2, p3, p4, p5])
+                    res = []
+                    chain = Chain(p, 5, inStream, res, phases)
+                    chain.compute()
+                    thrust = res[0]
+                    if thrust > maxThrust:
+                        maxThrust = thrust
+                        bestPhases = str(p1)+str(p2)+str(p3)+str(p4)+str(p5)
+                        print("Found new max. Phases: ", bestPhases,", thrust: ", maxThrust)
+                used.remove(p4)
+            used.remove(p3)
+        used.remove(p2)
+    used.remove(p1)
+print("Done. Phases: ", bestPhases,", thrust: ", maxThrust)
 
-phases = [1,0,4,3,2]
-phases = makeListOfLists(phases)
-
-chain = Chain(p, 5, inStream, None, phases)
-chain.compute()
 
