5d71acfb5d9550771882df8e1ba31c34a0c28c83
Sergey <kubiks@gmail.com>
2019-12-12 23:49:27 +0000

    Day 12 pt2 bruteforce xyz done

--- Day12.py
+++ Day12.py
@@ -1,5 +1,15 @@
 from NBodySimulator import NBodySimulator
 
+import math
+
+def isPrime(n):
+    if n < 2:
+        return False
+    for number in islice(count(2), int(sqrt(n) - 1)):
+        if n % number == 0:
+            return False
+    return True
+
 def main():
     b1 = [[-1, 0, 2],[2,-10,-7],[4,-8,8],[3,5,-1]]
     b2 = [[-8,-10,0],[5,5,10],[2,-7,3],[9,-8,-3]]
@@ -9,11 +19,23 @@ def main():
 #    for i in range(1000):
 #        system.calculate()
 #        print("After ", i+1, " steps:")
-#        for x in system.bodies:
-#            print("pos = ", x.pos.p, "    vel=", x.speed.p)
+    for x in system.bodies:
+        print("body =", x)
     print("Steps until repeated: ", numSteps)
     totalEnergy = system.calculateTotalEnergy()
     print("Total energy: ", totalEnergy)
 
+def lcm(a,b): return abs(a * b) / math.gcd(a,b) if a and b else 0
+
+
+def findRepetition():
+    x = 135024
+    y = 231614
+    z = 102356
+    xy = int(lcm(x,y))
+    print(xy)
+    xyz = int(lcm(xy,z))
+    print(xyz)
+
 if __name__ == '__main__':
-    main()
+    findRepetition()
--- NBodySimulator.py
+++ NBodySimulator.py
@@ -1,43 +1,43 @@
 import copy
 
-class Vector:
-    def __init__(self, x, y, z):
-        self.p = [x, y, z]
-
-    def equals(self, b):
-        for i in range(len(self.p)):
-            if self.p[i] != b.p[i]:
-                return False
-        return True
-
-
 class NBodySimulator:
 
     class Body:
-        def __init__(self, pos):
-            self.pos = pos
-            self.speed = Vector(0, 0, 0)
+        def __init__(self, x, y, z):
+            self.x = x
+            self.y = y
+            self.z = z
 
+            self.vx = 0
+            self.vy = 0
+            self.vz = 0
 
-        def applyGravityOf(self, b):
-            for i in range(len(self.pos.p)):
-                if self.pos.p[i] > b.pos.p[i]:
-                    self.speed.p[i] -= 1
-                elif self.pos.p[i] < b.pos.p[i]:
-                    self.speed.p[i] += 1
+        def equals(self, b):
+            return (self.x, self.y, self.z, self.vx, self.vy, self.vz) == (b.x, b.y, b.z, b.vx, b.vy, b.vz)
+
+        def applyGravityOf(self, b, mutual=False):
+            ax = 1 if self.x < b.x else -1 if self.x > b.x else 0
+            ay = 1 if self.y < b.y else -1 if self.y > b.y else 0
+            az = 1 if self.z < b.z else -1 if self.z > b.z else 0
+            self.vx += ax
+            self.vy += ay
+            self.vz += az
+            if mutual:
+                b.vx -= ax
+                b.vy -= ay
+                b.vz -= az
+
 
         def applyVelocity(self):
-            for i in range(len(self.pos.p)):
-                self.pos.p[i] += self.speed.p[i]
+            self.x += self.vx
+            self.y += self.vy
+            self.z += self.vz
 
     def calculateTotalEnergy(self):
         energy = 0
         for a in self.bodies:
-            potential = 0
-            kinetic = 0
-            for i in range(len(a.pos.p)):
-                potential += abs(a.pos.p[i])
-                kinetic += abs(a.speed.p[i])
+            potential = abs(a.x) + abs(a.y) + abs(a.z)
+            kinetic = abs(a.vx) + abs(a.vy) + abs(a.vz)
             energy += potential * kinetic
         return energy
 
@@ -48,36 +48,59 @@ class NBodySimulator:
             x = body[0]
             y = body[1]
             z = body[2]
-            self.bodies.append(self.Body(Vector(x, y, z)))
+            self.bodies.append(self.Body(x, y, z))
         self.initialBodies = copy.deepcopy(self.bodies)
         self.initialTotalEnergy = self.calculateTotalEnergy()
+        self.numBodies = len(self.bodies)
 
-    def sameAsInitial(self):
-        for i in range(len(self.bodies)):
-            if not self.bodies[i].pos.equals(self.initialBodies[i].pos):
-                return False
-            if not self.bodies[i].speed.equals(self.initialBodies[i].speed):
-                return False
+    def sameAsInitial(self, axis = None):
+        for i in range(self.numBodies):
+            if axis is None:
+                if not self.bodies[i].equals(self.initialBodies[i]):
+                    return False
+            elif axis == 0:
+                if self.bodies[i].x != self.initialBodies[i].x:
+                    return False
+                if self.bodies[i].vx != self.initialBodies[i].vx:
+                    return False
+            elif axis == 1:
+                if self.bodies[i].y != self.initialBodies[i].y:
+                    return False
+                if self.bodies[i].vy != self.initialBodies[i].vy:
+                    return False
+            elif axis == 2:
+                if self.bodies[i].z != self.initialBodies[i].z:
+                    return False
+                if self.bodies[i].vz != self.initialBodies[i].vz:
+                    return False
         return True
 
 
     def calculate(self, steps = 1, stopWhenRepeats = False):
         i = 0
+        fx = False
+        fy = False
+        fz = False
         while (i<steps) or ((steps == 0) and stopWhenRepeats):
-            for a in self.bodies:
-                for b in self.bodies:
-                    if a == b:
-                        continue
-                    a.applyGravityOf(b)
+            for a in range(self.numBodies-1):
+                for b in range(a+1, self.numBodies):
+                    self.bodies[a].applyGravityOf(self.bodies[b], True)
             for a in self.bodies:
                 a.applyVelocity()
             if stopWhenRepeats:
                 if i % 100000 == 0:
                     print("Done ", i, " steps")
-                if self.calculateTotalEnergy() == self.initialTotalEnergy:
-                    print("Same total energy after ", i, " steps")
-                if self.sameAsInitial():
+                if self.sameAsInitial() or (fx and fy and fz):
                     return i+1
+                if not fx and self.sameAsInitial(0):
+                    print("repeat X: ", i+1)
+                    fx = True
+                if not fy and self.sameAsInitial(1):
+                    print("repeat Y: ", i+1)
+                    fy = True
+                if not fz and self.sameAsInitial(2):
+                    print("repeat Z: ", i+1)
+                    fz = True
             i += 1
         return None
 
