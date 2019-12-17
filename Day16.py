fa4e9fd6705ca2a91e395c5ca206f1ae7ace3aa0
Sergey <kubiks@gmail.com>
2019-12-17 19:36:12 +0000

    Day16 done bruteforce with segments

--- Day16.py
+++ Day16.py
@@ -1,9 +1,10 @@
 from FlawedFrequencyTransmission import FFT
 
 def main():
-    #s1 = open("day16_input.txt", "r").read()
-    s1 = "03036732577212944063491565474664"
+    s1 = open("day16_input.txt", "r").read()
+    #s1 = "03036732577212944063491565474664"
     res = FFT.processMultipliedSignal(s1, 100, 8)
+    #res = FFT.processSignal(s1, 4)
     print("Result: ", res)
 
 if __name__ == '__main__':
--- FlawedFrequencyTransmission.py
+++ FlawedFrequencyTransmission.py
@@ -1,3 +1,6 @@
+import time
+
+
 class FFT:
 
     __pattern = (0, 1, 0, -1)
@@ -5,17 +8,53 @@ class FFT:
 
     @staticmethod
     def __processPhase(signal):
-        outSignal = [0] * len(signal)
-        for i in range(len(signal)):
-            patSize = len(FFT.__pattern)
-            longPatSize = patSize * (i+1)
-            res = 0
-            for j in range(len(signal)):
-                modifierPos = ((j+1) % longPatSize) // (i+1)
-                modifier = FFT.__pattern[modifierPos]
-                value = signal[j] * modifier
-                res += value
+        lenSignal = len(signal)
+        outSignal = [0] * lenSignal
+        startTime = time.time()
+        fullCalcCycles = 3
+        fullCalc = True
+        for i in range(lenSignal):
+            if i == fullCalcCycles: #lenSignal / 64:
+                fullCalc = False
+            if fullCalc:
+                res = 0
+            stop = False
+            startPlus = i
+            segments = []
+            while not stop:
+                endPlus = startPlus + i+1
+                startMinus = startPlus + (i+1)*2
+                endMinus = startMinus + i+1
+                if endMinus > lenSignal:
+                    endMinus = lenSignal
+                    if startMinus > lenSignal:
+                        startMinus = lenSignal
+                        if endPlus > lenSignal:
+                            endPlus = lenSignal
+                            if startPlus > lenSignal:
+                                startPlus = lenSignal
+                if not fullCalc or i == fullCalcCycles-1:
+                    segments.append([startPlus, endPlus, startMinus, endMinus])
+                if fullCalc:
+                    res = res + sum(signal[startPlus:endPlus]) - sum(signal[startMinus:endMinus])
+                startPlus += (i+1)*4
+                if startPlus >= lenSignal:
+                    stop = True
+            if not fullCalc:
+                for n in range(len(prevSegments)):
+                    if n >= len(segments):
+                        res -= sum(signal[prevSegments[n][0]:prevSegments[n][1]])
+                        res += sum(signal[prevSegments[n][2]:prevSegments[n][3]])
+                    else:
+                        res -= sum(signal[prevSegments[n][0]:min(segments[n][0], prevSegments[n][1])])
+                        res += sum(signal[max(prevSegments[n][1], segments[n][0]):segments[n][1]])
+                        res += sum(signal[prevSegments[n][2]:min(segments[n][2], prevSegments[n][3])])
+                        res -= sum(signal[max(prevSegments[n][3], segments[n][2]):segments[n][3]])
             outSignal[i] = abs(res) % 10
+            prevSegments = segments
+            #if i % 10000 == 0: print("Calculated ",i,"elements of",lenSignal)
+        elapsedTime = time.time() - startTime
+        print("Phase done. Elapsed time: ", elapsedTime)
         return outSignal
 
     @staticmethod
@@ -34,7 +73,7 @@ class FFT:
     @staticmethod
     def processMultipliedSignal(signal, numPhases, numReturnDigits = 0):
         offset = int(signal[:7])
-        signal = [int(char) for char in signal] * 100
+        signal = [int(char) for char in signal] * 10000
         for i in range(numPhases):
             signal = FFT.__processPhase(signal)
             print("Done ", i, " phase")
