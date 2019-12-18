054170bea93a8a2b7556cec7db66b39b5fe3af49
Sergey <kubiks@gmail.com>
2019-12-18 00:10:42 +0000

    Day17 todo

---
+++ Test_ASCII.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class ASCIITestCase(unittest.TestCase):
+    def test_day17(self):
+        #TODO: allow to run ASCII without graph for testing
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
